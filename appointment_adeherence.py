96b4bed66229c05ac861ccf7dd0325f4b60089e3
saurav1066 <sauravadhikari35@gmail.com>
2025-07-02 09:02:36 +0200

    fixed script to run on localhost

--- appointment_adeherence.py
+++ appointment_adeherence.py
@@ -1,3 +1,35 @@
 #%%
-import marimo as mo
-import pandas as pd
+
+import marimo
+
+__generated_with = "0.14.9"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import pandas as pd
+    return (pd,)
+
+
+@app.cell
+def _(pd):
+    df = pd.read_csv('data/medical_appointment.csv')
+    df.head()
+    return (df,)
+
+
+@app.cell
+def _(df):
+    df.describe()
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
