7e62c147d57dd97ba733a2f50065dc815892dd8f
saurav1066 <sauravadhikari35@gmail.com>
2025-07-03 08:54:58 +0200

    added dependencies and requirements

--- appointment_adeherence.py
+++ appointment_adeherence.py
@@ -1,5 +1,40 @@
 #%%
 
+# import marimo
+
+# __generated_with = "0.14.9"
+# app = marimo.App()
+
+
+# @app.cell
+# def _():
+#     import marimo as mo
+#     import pandas as pd
+#     return (pd,)
+
+
+# @app.cell
+# def _(pd):
+#     df = pd.read_csv('data/medical_appointment.csv')
+#     df.head()
+#     return (df,)
+
+
+# @app.cell
+# def _(df):
+#     df.describe()
+#     return
+
+
+# @app.cell
+# def _():
+#     return
+
+
+# if __name__ == "__main__":
+#     app.run()
+#
+
 import marimo
 
 __generated_with = "0.14.9"
@@ -10,12 +45,13 @@ app = marimo.App()
 def _():
     import marimo as mo
     import pandas as pd
+
     return (pd,)
 
 
 @app.cell
 def _(pd):
-    df = pd.read_csv('data/medical_appointment.csv')
+    df =pd.read_csv('data/medical_appointment.csv')
     df.head()
     return (df,)
 
@@ -23,6 +59,13 @@ def _(pd):
 @app.cell
 def _(df):
     df.describe()
+    return
+
+
+@app.cell
+def _(df):
+    # finding null values each column
+    df.isnull().sum()
     return
 
 
